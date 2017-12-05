d738cc3869a3081d7617b19104c8252e72f049ab
Anna Moskalchuk <anna@crystax.net>
2017-12-05 15:23:28 +0200

    Version 1.0

--- designs.js
+++ designs.js
@@ -9,22 +9,38 @@ function makeGrid() {
   $("tr").remove();
   $("td").remove();
 
-  var sizeHeight, sizeWidth;
-
-  sizeHeight = $("#input_height").val();
-  sizeWidth = $("#input_width").val();
+  const sizeHeight = $("#input_height").val();
+  const sizeWidth = $("#input_width").val();
 
   for (let i = 1; i <= sizeHeight; i++) {
+
     $("#pixel_canvas").append("<tr></tr");
     $("tr").addClass("tr0");
 
     for (let j = 1; j <= sizeWidth; j++) {
+
       $("#pixel_canvas").append("<td></td>");
       $("td").addClass("cell");
 
     }
   }
 }
+
+
+function choiceColor() {
+
+  return $("#colorPicker").val();
+
+};
+
+
+function makeColor(tdClick) {
+
+  const color = choiceColor();
+
+  $(tdClick).css("background-color", color);
+
+};
 
 
 $("#subButton").click(function(event) {
@@ -34,3 +50,15 @@ $("#subButton").click(function(event) {
   makeGrid ();
 
 });
+
+
+$("table").click(function(event) {
+
+  const target = event.target;
+
+  if (target.tagName != "TD") return;
+
+  makeColor(target);
+
+});
+
