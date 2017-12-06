2ed88002885c96bddca0cca0440d6b7769527854
Anna Moskalchuk <anna@crystax.net>
2017-12-06 14:38:27 +0200

    add comments

--- designs.js
+++ designs.js
@@ -3,62 +3,84 @@
 
 // When size is submitted by the user, call makeGrid()
 
+/**
+* @description Adds cells at table
+* @description Adds classes at cells
+*/
 
 function makeGrid() {
 
-  $("tr").remove();
-  $("td").remove();
+    $("tr").remove();
+    $("td").remove();
 
-  const sizeHeight = $("#input_height").val();
-  const sizeWidth = $("#input_width").val();
+    const sizeHeight = $("#input_height").val();
+    const sizeWidth = $("#input_width").val();
 
-  for (let i = 1; i <= sizeHeight; i++) {
+    for (let i = 1; i <= sizeHeight; i++) {
 
-    $("#pixel_canvas").append("<tr></tr");
-    $("tr").addClass("trHeight0");
+        $("#pixel_canvas").append("<tr></tr");
+        $("tr").addClass("trHeight0");
 
-    for (let j = 1; j <= sizeWidth; j++) {
+      for (let j = 1; j <= sizeWidth; j++) {
 
-      $("#pixel_canvas").append("<td></td>");
-      $("td").addClass("cell");
+          $("#pixel_canvas").append("<td></td>");
+          $("td").addClass("cell");
 
-    }
-  }
-}
+    };
+  };
+};
 
+
+/**
+* @description Takes color from input id = "colorPicker"
+* @returns {color} Color of input
+*/
 
 function choiceColor() {
 
-  return $("#colorPicker").val();
+    return $("#colorPicker").val();
 
 };
 
+
+/**
+* @description Changes background color of choiced cell
+* @param {object} tdClick it is cell
+*/
 
 function makeColor(tdClick) {
 
-  const color = choiceColor();
+    const color = choiceColor();
 
-  $(tdClick).css("background-color", color);
+    $(tdClick).css("background-color", color);
 
 };
 
 
+//TODO: Listenes to click on button
+//TODO: Stopes default actions of brouser
+//TODO: Calls function makeGrid
+
 $("#subButton").click(function(event) {
 
-  event.preventDefault();
+    event.preventDefault();
 
-  makeGrid ();
+    makeGrid ();
 
 });
 
 
+//TODO: Listenes to click on table
+//TODO: Determines the cell
+//TODO: Calls function makeColor
+
 $("table").click(function(event) {
 
-  const target = event.target;
+    const target = event.target;
 
-  if (target.tagName != "TD") return;
+    if (target.tagName != "TD") return;
 
-  makeColor(target);
+    makeColor(target);
 
 });
 
